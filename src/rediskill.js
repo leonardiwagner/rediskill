ed4c3dc29a40b6701d3e5658678d3f67c3fe2975
Wagner Leonardi <leonardiwagner@gmail.com>
2016-06-03 11:08:35 -0300

    Update rediskill.js

--- src/rediskill.js
+++ src/rediskill.js
@@ -6,8 +6,19 @@ var rediskill = function(host, port){
   var messageSender = require('./messageSender')(connection, QUEUE_NAME);
   var messageReceiver = require('./messageReceiver')(connection, QUEUE_NAME);
 
+  var sendMessages = function(messages){
+    var sendMessagesFunctions = messages.map(function(message){
+      return messageSender.send(message.key, message.value);
+    });
+
+    return Promise.all(sendMessagesFunctions);
+  }
+
   return {
     sendMessage: messageSender.send,
+    sendMessages: sendMessages,
     receiveMessage: messageReceiver.receive
   }
 };
+
+module.exports = rediskill;
